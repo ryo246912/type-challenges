d528ac4ee199a0cf4192e5691b4a0e1d38216ea4
ryo246912 <r-k.sky_25cloudf@hotmail.co.jp>
2025-02-24 14:01:37 +0900

    chore: `00014-easy-first-of-array.ts`

--- playground/easy/00014-easy-first-of-array.ts
+++ playground/easy/00014-easy-first-of-array.ts
@@ -22,7 +22,13 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type First<T extends any[]> = any
+type First<T extends any[]> = T extends [] ? never : T[0]
+
+//answer2
+// type First<T extends any[]> = T['length'] extends 0 ? never : T[0]
+
+//answer3
+// type First<T extends any[]> = T extends [infer A, ...infer rest] ? A : never
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from '../../utils'
